051275de28584cc9f6b5ac0f39236f924f195d18
Robert Aguirre <RobertAguirre@users.noreply.github.com>
2017-07-26 00:51:07 -0400

    Added Player Bar Controller, and addressed play, pause, and previous methods

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,11 +1,15 @@
 (function() {
-    function SongPlayer() {
+    function SongPlayer(Fixtures) {
 
         //*** Private Attributes *** //
 
         var SongPlayer = {};
 
-        var currentSong = null;
+        /**
+        *@desc Information from current album
+        *@type {Object}
+        */
+        var currentAlbum = Fixtures.getAlbum();
 
         /**
         * @desc Buzz object audio file
@@ -23,7 +27,7 @@
         var setSong = function(song) {
           if (currentBuzzObject) {
               currentBuzzObject.stop();
-              currentSong.playing = null;
+              SongPlayer.currentSong.playing = null;
           }
           currentBuzzObject = new buzz.sound(song.audioUrl, {
               formats: ['mp3'],
@@ -31,7 +35,7 @@
               volume: 10
           });
 
-          currentSong = song;
+          SongPlayer.currentSong = song;
         };
 
         /**
@@ -44,28 +48,77 @@
           song.playing = true;
         };
 
+        /**
+        * @function getSongIndex
+        * @desc Get index of song in the songs array
+        * @param {Object} song
+        * @returns {Number}
+        */
+        var getSongIndex = function(song){
+            return currentAlbum.songs.indexOf(song);
+        };
 
+        //***** Public Attributes *****//
 
-         SongPlayer.play = function(song) {
-            if (currentSong !== song) { //currently playing song is not equal to song clicked on
+        /**
+        * @desc Active song object from list of songs
+        * @type {Object}
+        */
+        SongPlayer.currentSong = null;
+
+        //***** Public Functions *****//
+
+        /**
+        * @function play
+        * @desc Play current or new song
+        * @param {Object} song
+        */
+        SongPlayer.play = function(song) {
+          song = song || SongPlayer.currentSong;
+          if (SongPlayer.currentSong !== song) { //currently playing song is not equal to song clicked on
+            setSong(song);
+            playSong(song);
+          } else if (SongPlayer.currentSong === song) {
+            if (currentBuzzObject.isPaused()) {
+            playSong(song);
+            }
+          }
+        };
+
+        /**
+        * @function pause
+        * @desc Pause current song
+        * @param {Object} song
+        */
+        SongPlayer.pause = function(song) {
+          song = song || SongPlayer.currentSong;
+          currentBuzzObject.pause();
+          song.playing = false;
+        };
+
+        /**
+        *@function Player bar previous button
+        *@desc Sets song to previous song in album
+        *
+        */
+        SongPlayer.previous = function() {
+          var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+          currentSongIndex--;
+
+          if (currentSongIndex < 0) {
+              currentBuzzObject.stop();
+              SongPlayer.currentSong.playing = null;
+          } else {
+              var song = currentAlbum.songs[currentSongIndex];
               setSong(song);
               playSong(song);
-            } else if (currentSong === song) {
-              if (currentBuzzObject.isPaused()) {
-              playSong(song);
-              }
-            }
-          };
+          }
+        };
 
-         SongPlayer.pause = function(song) {
-           currentBuzzObject.pause();
-           song.playing = false;
-         };
-
-         return SongPlayer;
+        return SongPlayer;
     }
 
     angular
         .module('blocJams')
-        .factory('SongPlayer', SongPlayer);
-})();
+        .factory('SongPlayer', ['Fixtures', SongPlayer]);
+  })();
